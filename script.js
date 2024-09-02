264175505479988ef023524880f1245485796996
Johann Roth <johann.roth.e46@googlemail.com>
2024-09-02 20:47:15 +0200

    add the template.js

--- script.js
+++ script.js
@@ -32,38 +32,6 @@ async function renderListOfPokemons(array) {
     }
 }
 
-// Erstellen der einzelnen Karten für die Pokemon
-function generateTable(i, pokemonObj) {
-    let mainType = pokemonObj.types[0].type.name;
-
-    return /*html*/`
-    <div id="pokeCard${i}" class="pokeCard">
-        <div class="card type-${mainType}">
-            <h5 class="card-title">
-                <p>#${pokemonObj.id}</p>
-                <p>${pokemonObj.name}</p>
-            </h5>
-            <div class="card-img">
-                <img src="${pokemonObj.sprites.other.showdown.front_default}" class="poke-img">
-            </div>
-            <div class="card-body">
-                <div class="poke-card-type-box">
-                    ${renderPokemonTypes(pokemonObj.types)}
-                </div>
-            </div>
-        </div>
-    </div>
-    `;
-}
-
-// Typen anzeigen und Hintergrundfarbe setzen
-function renderPokemonTypes(types) {
-    return types.map(typeInfo => {
-        let typeName = typeInfo.type.name;
-        return `<span class="badge type-${typeName}">${typeName}</span>`;
-    }).join(' ');
-}
-
 // Funktion zum Anzeigen des Lade-Screens
 function showLoadingScreen() {
     document.getElementById('loading-screen').style.display = 'flex';
